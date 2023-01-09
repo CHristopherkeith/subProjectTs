2bd2bc21ee37b9b09137b1bfdf99dcf709069725
Christopher <easonmu@hotmail.com>
2023-01-10 00:00:46 +0800

    feat: init: init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App 66"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld1 from '../components/HelloWorld.vue'; 
+import AboutView from './AboutView.vue'; 
+
+// export default defineComponent({
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld,
+//   },
+// });
+</script>
---
+++ src/views/SubProjectTsHomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>HomeView7 page from this {{ title }}</h1>
+    <div>-------------------------------------------------------------------</div>
+    <HomeView/>
+  </div>
+  
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref } from "setupDemo/node_modules/vue";
+// import HomeView from "@/views/HomeView/HomeView.vue";
+import HomeView from "setupDemo/src/views/HomeView/HomeView.vue";
+
+const title: Ref<string> = ref("");
+
+title.value = "subProjectTs";
+</script>
